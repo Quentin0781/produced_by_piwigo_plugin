3d8d711796bac611048b923ad73513619c4380a8
Quentin0781 <37435744+Quentin0781@users.noreply.github.com>
2018-12-18 10:00:33 +0100

    updated version main.inc.php

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Show photo produced by
-Version: 1.0.0
+Version: 1.0.1
 Description: produced by photo
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=790
 Author: plg
@@ -19,17 +19,23 @@ if (!defined('PHPWG_ROOT_PATH'))
 
 global $prefixeTable;
 
-defined('SPI_ID') or define('SPI_ID', basename(dirname(__FILE__)));
-define('SPI_PATH' , PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
-define('SPI_VERSION', '2.8.a');
+//defined('SPI_ID') or define('SPI_ID', basename(dirname(__FILE__)));
+defined('PRODUCED_BY_ID') or define('PRODUCED_BY_ID', basename(dirname(__FILE__)));
+
+//define('SPI_PATH' , PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
+define('PRODUCED_BY_PATH', PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
+
+//define('SPI_VERSION', '2.8.a');
+define('PRODUCED_BY_VERSION', '1.0.1');
+
 
 // init the plugin
-add_event_handler('init', 'spi_init');
+add_event_handler('init', 'produced_by_init');
 /**
  * plugin initialization
  *   - load language
  */
-function spi_init()
+function produced_by_init()
 {
   if (script_basename() == 'picture')
   {
@@ -39,28 +45,28 @@ function spi_init()
 }
 
 
-add_event_handler('loc_end_picture', 'spi_end_picture');
-function spi_end_picture()
+add_event_handler('loc_end_picture', 'produced_by_end_picture');
+function produced_by_end_picture()
 {
   global $template, $picture;
 
-  $template->set_prefilter('picture', 'spi_picture_prefilter');
+  $template->set_prefilter('picture', 'produced_by_picture_prefilter');
 
   $template->assign(
     array(
-      'IMAGE_ID' => $picture['current']['produced_by'],
+      'IMAGE_PRODUCED_BY' => $picture['current']['produced_by'],
       )
     );
 }
 
-function spi_picture_prefilter($content, &$smarty)
+function produced_by_picture_prefilter($content, &$smarty)
 {
   $search = '{if $display_info.created_on and isset($INFO_CREATION_DATE)}';
   
   $replace = '
 	<div id="ImageId" class="imageInfo">
 		<dt>{"Produced by"}</dt>
-		<dd>{$IMAGE_ID}</dd>
+		<dd>{$IMAGE_PRODUCED_BY}</dd>
 	</div>
 '.$search;
   
